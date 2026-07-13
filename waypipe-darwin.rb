ecd3a991f7ba1a112ec441ffd746f1161d836062
J-x-Z <39978180+J-x-Z@users.noreply.github.com>
2026-07-13 08:37:35 +0800

    Update Homebrew formula for Waypipe 0.11

---
+++ waypipe-darwin.rb
@@ -0,0 +1,25 @@
+class WaypipeDarwin < Formula
+  desc "Proxy for Wayland clients with macOS transport support"
+  homepage "https://github.com/J-x-Z/waypipe-darwin"
+  url "https://github.com/J-x-Z/waypipe-darwin.git", branch: "main"
+  version "0.11.0-darwin"
+  head "https://github.com/J-x-Z/waypipe-darwin.git", branch: "main"
+
+  depends_on "rust" => :build
+  depends_on "lz4"
+  depends_on "zstd"
+
+  conflicts_with "waypipe", because: "both install a waypipe binary"
+
+  def install
+    system "cargo", "install", *std_cargo_args,
+           "--no-default-features", "--features", "lz4,zstd"
+  end
+
+  test do
+    output = shell_output("#{bin}/waypipe --version")
+    assert_match "waypipe 0.11.0", output
+    assert_match "lz4: true", output
+    assert_match "zstd: true", output
+  end
+end
