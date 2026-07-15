99522d966db86ffa45f4c2c3e2ee91ea58115040
J-x-Z <39978180+J-x-Z@users.noreply.github.com>
2026-07-15 10:16:02 +0800

    Pin the Homebrew package source

--- waypipe-darwin.rb
+++ waypipe-darwin.rb
@@ -1,8 +1,9 @@
 class WaypipeDarwin < Formula
   desc "Proxy for Wayland clients with macOS transport support"
   homepage "https://github.com/J-x-Z/waypipe-darwin"
-  url "https://github.com/J-x-Z/waypipe-darwin.git", branch: "main"
-  version "0.11.0-darwin"
+  url "https://github.com/J-x-Z/waypipe-darwin/archive/refs/tags/v0.11.0-darwin.1.tar.gz"
+  version "0.11.0-darwin.1"
+  sha256 "5d1d0f1c384a1ca0fc7ef492ca73055343842fde4856cdbb77a7c2a2836cd6fa"
   head "https://github.com/J-x-Z/waypipe-darwin.git", branch: "main"
 
   depends_on "bindgen" => :build
@@ -11,10 +12,10 @@ class WaypipeDarwin < Formula
   depends_on "zstd"
 
   def install
-    ENV.prepend_path "PATH", Formula["bindgen"].opt_bin
-    ENV.prepend_path "PATH", Formula["rust"].opt_bin
-    ENV.prepend_path "PKG_CONFIG_PATH", Formula["lz4"].opt_lib/"pkgconfig"
-    ENV.prepend_path "PKG_CONFIG_PATH", Formula["zstd"].opt_lib/"pkgconfig"
+    ENV.prepend_path "PATH", formula_opt_bin("bindgen")
+    ENV.prepend_path "PATH", formula_opt_bin("rust")
+    ENV.prepend_path "PKG_CONFIG_PATH", formula_opt_lib("lz4")/"pkgconfig"
+    ENV.prepend_path "PKG_CONFIG_PATH", formula_opt_lib("zstd")/"pkgconfig"
     system "cargo", "install", *std_cargo_args,
            "--no-default-features", "--features", "lz4,zstd"
   end
